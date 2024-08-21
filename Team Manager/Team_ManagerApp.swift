f9654bfb7be6d0a5e821e1f4725d1da3528d08ae
Michael Gibson <michaelgibson1029@gmail.com>
2024-08-21 15:35:38 -0700

    Initial Commit

---
+++ Team Manager/Team_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team_ManagerApp.swift
+//  Team Manager
+//
+//  Created by Michael Gibson on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Team_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
