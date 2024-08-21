57092672af74417b9634f02ec935b67322c9c35f
Michael Gibson <michaelgibson1029@gmail.com>
2024-08-21 16:00:50 -0700

    initialized project, setup firebase and auth system

---
+++ Team Manager/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Team Manager
+//
+//  Created by Michael Gibson on 8/21/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
---
+++ Team Manager/Views/Authentication/AuthView.swift
@@ -0,0 +1,122 @@
+//
+//  AuthView.swift
+//  Team Manager
+//
+//  Created by Michael Gibson on 8/21/24.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    
+    @State private var errorMessage: String?
+    @State private var isLoading: Bool = false
+    
+    @EnvironmentObject private var authController: AuthController
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Text("Welcome Back!")
+                .font(.largeTitle)
+                .foregroundColor(Color("CharcoalBlack"))
+                .fontWeight(.bold)
+                .padding(.bottom, 40)
+            
+            TextField("Email", text: $authController.email)
+                .textCase(.lowercase)
+                .padding()
+                .background(Color("VeryLightGray"))
+                .cornerRadius(10)
+                .foregroundColor(.black)
+                .padding(.bottom, 20)
+            
+            
+            SecureField("Password", text: $authController.password)
+                .textCase(.lowercase)
+                .padding()
+                .background(Color("VeryLightGray"))
+                .cornerRadius(10)
+                .foregroundColor(.black)
+                .padding(.bottom, 20)
+            
+            Button(action: signIn) {
+                Text("Login")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color("EmeraldGreen"))
+                    .cornerRadius(10)
+            }
+            .padding(.top, 20)
+            
+            Button(action: signOut) {
+                Text("Forgot Password?")
+                    .font(.footnote)
+                    .foregroundColor(Color("DeepSkyBlue"))
+            }
+            .padding(.top, 10)
+            
+            Spacer()
+            
+            HStack {
+                Text("Don't have an account?")
+                    .foregroundColor(Color("CharcoalBlack"))
+                Button(action: signUp) {
+                    Text("Sign Up")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("DeepSkyBlue"))
+                }
+            }
+            .padding(.top, 20)
+        }
+        .padding()
+        .background(Color("WhiteSmoke"))
+    }
+    
+    func signUp() {
+        Task {
+            do {
+                try await authController.signUp()
+            } catch {
+                errorMessage = error.localizedDescription
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func signIn() {
+        Task {
+            do {
+                try await authController.signIn()
+            } catch {
+                errorMessage = error.localizedDescription
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func signOut() {
+        Task {
+            do {
+                try authController.signOut()
+            } catch {
+                errorMessage = error.localizedDescription
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+    
+ 
+
+    struct AuthView_Previews: PreviewProvider {
+        static var previews: some View {
+            AuthView()
+                .environmentObject(AuthController())
+        }
+    }
+
+
